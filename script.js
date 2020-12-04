f629cc704b495952246bd5accbe5ec2fcae0a7c7
deliaturner <deliajturner@gmail.com>
2020-12-03 22:43:23 -0500

    all parts working

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-function add() {
-  const num1 = parseInt(document.getElementById("first").value);
-  const num2 = parseInt(document.getElementById("second").value);
-  const total = num1 + num2;
-  document.getElementById("total").innerText = total;
+function addition() {
+  let num1 = parseInt(document.getElementById("add1").value);
+  let num2 = parseInt(document.getElementById("add2").value);
+  let answer = document.getElementById("addTotal");
+  let totalAnswer = num1 + num2;
+  answer.value = totalAnswer;
 }
-
-function subtract(num1, num2) {}
-
-function multiply(num1, num2) {}
-
-function divide(num1, num2) {}
+function subtraction() {
+  let num1 = document.getElementById("sub1").value;
+  let num2 = document.getElementById("sub2").value;
+  let answer = document.getElementById("subTotal");
+  let totalAnswer = num1 - num2;
+  answer.value = totalAnswer;
+}
+function multiplication() {
+  let num1 = document.getElementById("mult1").value;
+  let num2 = document.getElementById("mult2").value;
+  let answer = document.getElementById("multTotal");
+  let totalAnswer = num1 * num2;
+  answer.value = totalAnswer;
+}
+function division() {
+  let num1 = document.getElementById("div1").value;
+  let num2 = document.getElementById("div2").value;
+  let answer = document.getElementById("divTotal");
+  let totalAnswer = num1 / num2;
+  answer.value = totalAnswer;
+}
