e94ed2de38cc2a845b3f597064466a9c1fa153fb
deliaturner <deliajturner@gmail.com>
2020-12-03 21:36:31 -0500

    addition function. not pretty

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let num1 = document.getElementById("first");
-let num2 = document.getElementById("second");
-
-function add(num1, num2) {
-  return num1 + num2;
+function add() {
+  const num1 = parseInt(document.getElementById("first").value);
+  const num2 = parseInt(document.getElementById("second").value);
+  const total = num1 + num2;
+  document.getElementById("total").innerText = total;
 }
+
+function subtract(num1, num2) {}
+
+function multiply(num1, num2) {}
+
+function divide(num1, num2) {}
